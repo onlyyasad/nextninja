9d3d73a084417d7e5cd91dd1b3a9621407482378
Hafij Al Asad <onlyyasad@gmail.com>
2023-08-05 09:00:50 +0600

    with layout and without layout pages implemented

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,26 +4,51 @@ import Link from 'next/link';
 
 
 const NavBar = () => {
+
+    const navMenu = [
+        {
+            id: 1,
+            path: "/",
+            title: "Home"
+        },
+
+        {
+            id: 2,
+            path: "/about",
+            title: "About"
+        },
+        {
+            id: 3,
+            path: "/blog",
+            title: "Blog"
+        },
+        {
+            id: 4,
+            path: "/dashboard",
+            title: "Dashboard"
+        },
+        {
+            id: 5,
+            path: "/contact-us",
+            title: "Contact Us"
+        }
+
+    ]
     return (
         <div className='bg-gray-200'>
             <div className='container mx-auto p-4 flex justify-between items-center'>
                 <div>
                     <h1 className='font-bold'>NextNinja</h1>
                 </div>
-                <div className='flex justify-between items-center'>
-                    <Button variant="link" asChild>
-                        <Link href="/">Home</Link>
-                    </Button>
-                    <Button variant="link" asChild>
-                        <Link href="/about">About Us</Link>
-                    </Button>
-                    <Button variant="link" asChild>
-                        <Link href="/blog">Blog</Link>
-                    </Button>
-                    <Button variant="link" asChild>
-                        <Link href="/contact-us">Contact Us</Link>
-                    </Button>
-                </div>
+                <ul className='flex justify-between items-center'>
+                    {
+                        navMenu.map(({id, path, title}) => <li key={id}>
+                            <Button variant="link" asChild>
+                                <Link href={path}>{title}</Link>
+                            </Button>
+                        </li>)
+                    }
+                </ul>
                 <div>
                     <Button variant="default">Login</Button>
                 </div>
